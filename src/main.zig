85aaaf9d875da0e75869e2b0c42a40fb599fc293
Vinod Halaharvi <vinod.halaharvi@gmail.com>
2025-12-26 17:47:12 +0530

    Add setup target to Makefile for fetching microzig

---
+++ src/main.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+const microzig = @import("microzig");
+const rp2xxx = microzig.hal;
+const time = rp2xxx.time;
+
+// Compile-time pin configuration
+const pin_config = rp2xxx.pins.GlobalConfiguration{
+    .GPIO15 = .{
+        .name = "led",
+        .direction = .out,
+    },
+};
+
+const pins = pin_config.pins();
+
+pub fn main() !void {
+    pin_config.apply();
+
+    while (true) {
+        pins.led.toggle();
+        time.sleep_ms(250);
+    }
+}
